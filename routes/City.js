149f8e6126a3d80f4754b9dab6174e0048453dad
purevb <purevbaatar221@gmail.com>
2024-04-30 10:30:35 +0800

    cityg update bolon delete hiine

--- routes/City.js
+++ routes/City.js
@@ -28,7 +28,7 @@ router.post("/city", async (req, res) => {
 // Get all posts
 router.get("/city", async (req, res) => {
   try {
-    Event.find()
+    City.find()
       .then((citys) => {
         console.log(citys);
         res.status(200).json({ citys: citys });
@@ -36,9 +36,64 @@ router.get("/city", async (req, res) => {
       .catch((error) => {
         res.status(500).json({ msg: "Unable to get city" });
       });
-  } catch (error) {
+  } catch (error) { 
     console.log(error);
     res.status(500).json({ msg: "Unable to get city" });
+  }
+});
+
+// update 
+router.get('/city/:id', async (req, res) => {
+  try {
+      const id = req.params.id;
+      const updatedCity = req.body;
+       await City.findOneAndUpdate({_id:id},updatedCity,{new:true})
+       .then((updatedCity)=>{
+          console.log(updatedCity);
+          res.status(200).json({msg:"City succesfully updated", city: City});
+       }).catch((error)=>{
+          console.log(City);
+          res.status(200).json({ City: City });    
+       })
+  } catch (error) {
+      console.log(error);
+      res.status(500).json({ msg: "Unable to find the city" });
+  }
+});
+// search
+router.get('/search', async (req, res) => {
+  try {
+      const searchTerm = req.query.searchTerm;
+      const searchRegex = new RegExp(searchTerm, "i");
+      const citys = await City.find({
+          $or: [
+              { cityId: searchRegex },
+              { cityName: searchRegex }
+          ]
+      });
+      console.log(citys);
+      res.status(200).json({ citys: citys });
+  } catch (error) {
+      console.log(error);
+      res.status(500).json({ msg: "Unable to find the Citys" });
+  }
+});
+
+
+
+router.delete('/city/:id', async (req, res) => {
+  try {
+      const id = req.params.id;
+      const deletedCity= await City.findOneAndDelete(id);
+      if(!deletedCity){
+        return res.status(404).json({ msg: "City not found" });
+      }
+      console.log(deletedCity);
+      res.status(200).json({msg:"City succesfully deleted"});
+       
+  } catch (error) {
+      console.log(error);
+      res.status(500).json({ msg: "unable to delete the city" });
   }
 });
 
